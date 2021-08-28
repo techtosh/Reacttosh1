316490548845d04ab42054f768a9980227780475
internship311 <techtosh588@gmail.com>
2021-08-28 23:17:05 +0530

    Transferred all the data fetching logic to useHomeFetch custom Hook

--- react_movie/src/components/Home.js
+++ react_movie/src/components/Home.js
@@ -1,23 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import API from '../API';
+import React from 'react';
 
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
+import { useHomeFetch } from '../hooks/useHomeFetch';
 
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-    const[state, setState] = useState();
+
+    const {state, loading, error} = useHomeFetch();
+
+    console.log(state);
+    /*const[state, setState] = useState();
     const[loading, setLoading] = useState(false);
     const[error, setError] = useState(false);
-
     const fetchMovies = async (page, searchTerm = '') => {
         try {
             setError(false);
             setLoading(true);
-
             const movies = await API.fetchMovies(searchTerm, page);
             //console.log(movies);
-
             setState(prev => ({
                 ...movies,
                 results: page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
@@ -27,14 +28,13 @@ const Home = () => {
         }
         setLoading(false);
     }
-    
+ 
     //Will trigger on initial render
     useEffect(() => {
         fetchMovies(1);
     }, [])
-
     console.log(state);
-
+*/
     return(<div>Home Page</div>)
 }
 
