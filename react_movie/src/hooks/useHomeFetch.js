f8242430d932ddf4e08f054c64b1fe704b8cca88
internship311 <techtosh588@gmail.com>
2021-08-28 23:02:01 +0530

    Created a custom hook useHomeFetch.js

---
+++ react_movie/src/hooks/useHomeFetch.js
@@ -0,0 +1,43 @@
+import { useState, useEffect, useRef } from "react";
+//Import API
+import API from '../API';
+
+const initialState = {
+    page: 0,
+    results: [],
+    total_pages: 0,
+    total_results: 0
+};
+
+export const useHomeFetch = () => {
+    const[state, setState] = useState(initialState);
+    const[loading, setLoading] = useState(false);
+    const[error, setError] = useState(false);
+
+    const fetchMovies = async (page, searchTerm = '') => {
+        try {
+            setError(false);
+            setLoading(true);
+
+            const movies = await API.fetchMovies(searchTerm, page);
+            //console.log(movies);
+
+            setState(prev => ({
+                ...movies,
+                results: page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
+            }))
+        } catch(error) {
+            setError(true);
+        }
+        setLoading(false);
+    }
+
+    //Will trigger on initial render
+    useEffect(() => {
+        fetchMovies(1);
+    }, [])
+
+    //console.log(state);
+
+    return {state, loading, error};
+}
