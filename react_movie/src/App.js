753db5fe6f6c5f177ad22b3b231cf9484bb5bb94
internship311 <techtosh588@gmail.com>
2021-08-28 21:35:13 +0530

    Home component added

--- react_movie/src/App.js
+++ react_movie/src/App.js
@@ -1,12 +1,13 @@
 //import './App.css';
 import Header from "./components/Header";
 import { GlobalStyle } from "./GlobalStyle";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      Start here.
+      <Home />
       <GlobalStyle />
     </div>
   );
