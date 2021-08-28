00fcb45f65b4ac4e31b5f6b13c192864a9a32a17
internship311 <techtosh588@gmail.com>
2021-08-28 23:28:58 +0530

    Props are sent from Home.js to HeroImage component

--- react_movie/src/components/Home.js
+++ react_movie/src/components/Home.js
@@ -4,6 +4,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 import { useHomeFetch } from '../hooks/useHomeFetch';
 
 import NoImage from '../images/no_image.jpg';
+import HeroImage from './HeroImage';
 
 const Home = () => {
 
@@ -35,7 +36,18 @@ const Home = () => {
     }, [])
     console.log(state);
 */
-    return(<div>Home Page</div>)
+
+    return(
+        <>
+        {state.results[0] ? 
+        <HeroImage image= {`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`} 
+        title= {state.results[0].original_title}
+        text= {state.results[0].overview}
+        /> 
+        : 
+        null}
+        </>
+    )
 }
 
 export default Home;
