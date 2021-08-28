d4cee230b376be8ddf821fc4ba2f5f1902b3ff34
internship311 <techtosh588@gmail.com>
2021-08-28 23:35:09 +0530

    Grid component created

--- react_movie/src/components/Home.js
+++ react_movie/src/components/Home.js
@@ -5,6 +5,7 @@ import { useHomeFetch } from '../hooks/useHomeFetch';
 
 import NoImage from '../images/no_image.jpg';
 import HeroImage from './HeroImage';
+import Grid from './Grid';
 
 const Home = () => {
 
@@ -42,10 +43,14 @@ const Home = () => {
         {state.results[0] ? 
         <HeroImage image= {`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`} 
         title= {state.results[0].original_title}
-        text= {state.results[0].overview}
-        /> 
-        : 
-        null}
+        text= {state.results[0].overview} 
+        /> : null}
+
+        <Grid header= 'Popular Movies'>
+            {state.results.map(movie => (
+                <div key= {movie.id}>{movie.title}</div>
+            ))}
+        </Grid>
         </>
     )
 }
