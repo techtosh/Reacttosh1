abf72e6ff06c77e519163e32d93e92f6f7fbb651
internship311 <techtosh588@gmail.com>
2021-08-29 08:25:38 +0530

    React router established

--- react_movie/src/App.js
+++ react_movie/src/App.js
@@ -2,14 +2,22 @@
 import Header from "./components/Header";
 import { GlobalStyle } from "./GlobalStyle";
 import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Movie from "./components/Movie";
+import NotFound from "./components/NotFound";
 
 const App = () => {
+  
   return (
-    <div className="App">
+    <Router>
       <Header />
-      <Home />
+      <Routes>
+        <Route path= '/' element= {<Home />}/>
+        <Route path= '/:movieId' element= {<Movie />}/>
+        <Route path= '/*' element= {<NotFound />}/>
+      </Routes>
       <GlobalStyle />
-    </div>
+    </Router>
   );
 }
 
---
+++ react_movie/src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return(
+        <div>
+            Movie
+        </div>
+    )
+}
+
+export default NotFound;
