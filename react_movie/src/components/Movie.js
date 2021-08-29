8b1e5b7e27b9c654b1bb66879bfbbd1a3eee50ce
internship311 <techtosh588@gmail.com>
2021-08-29 08:41:31 +0530

    Actor component created

--- react_movie/src/components/Movie.js
+++ react_movie/src/components/Movie.js
@@ -12,6 +12,7 @@ import NoImage from '../images/no_image.jpg';
 import BreadCrumb from './BreadCrumb';
 import MovieInfo from './MovieInfo';
 import MovieInfoBar from './MovieInfoBar';
+import Actor from './Actor';
 
 const Movie = () => {
     const { movieId } = useParams();
@@ -36,6 +37,15 @@ const Movie = () => {
         <BreadCrumb movieTitle= {movie.original_title} />
         <MovieInfo movie= {movie} />
         <MovieInfoBar time= {movie.runtime} budget= {movie.budget} revenue= {movie.revenue}/>
+        <Grid header= "Actors">
+            {movie.actors.map(actor => (
+                <Actor key= {actor.credit_id} name= {actor.name} character= {actor.character} imageUrl= {
+                    actor.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                    : NoImage
+                }
+                />
+            ))}
+        </Grid>
     </>
     )
 }
--- react_movie/src/hooks/useHomeFetch.js
+++ react_movie/src/hooks/useHomeFetch.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 //Import API
 import API from '../API';
 
