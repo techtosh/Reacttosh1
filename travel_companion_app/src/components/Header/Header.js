19ed0d734790034564ffff16612b21dacf4eea6d
internship311 <techtosh588@gmail.com>
2021-08-30 22:31:31 +0530

    Map component created

--- travel_companion_app/src/components/Header/Header.js
+++ travel_companion_app/src/components/Header/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
         <AppBar position= "static">
             <Toolbar className= {classes.toolbar}>
                 <Typography variant= "h5" className= {classes.title}>
-                    Travel Advisor
+                    Travel Guide
                 </Typography>
                 <Box display= "flex">
                     <Typography variant= "h6" className= {classes.title}>
