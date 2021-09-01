f7543e80a1ed9f886ca9ca6f849544d024f28d11
internship311 <techtosh588@gmail.com>
2021-09-01 13:56:36 +0530

    Public and private routes are created

--- chat_message_app/src/App.js
+++ chat_message_app/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-//import logo from './logo.svg';
-//import './App.css';
+import { Switch } from 'react-router';
+
 import 'rsuite/dist/styles/rsuite-default.css';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
 import './styles/main.scss';
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Switch>
+      <PublicRoute path= "/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path= "/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
   );
 }
 
---
+++ chat_message_app/src/components/PublicRoute.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router';
+
+const PublicRoute = ({ children, ...routeProps }) => {
+
+    const profile = false;
+
+    if(profile) {
+        return <Redirect to= "/signin"/>
+    }
+
+    return(
+        <Route {...routeProps}>{children}</Route>
+    )
+}
+
+export default PublicRoute;
