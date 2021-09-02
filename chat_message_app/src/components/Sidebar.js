fb3a6370610a5c933f367a35f718cfcc99c37879
internship311 <techtosh588@gmail.com>
2021-09-02 13:48:13 +0530

    Sidebar component created along with the creation of custom hook

---
+++ chat_message_app/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DashboardToggle from './dashboard/DashboardToggle';
+
+const Sidebar = () => (
+    <div className= "h-100 pt-2">
+        <div>
+            <DashboardToggle />
+        </div>
+        bottom
+    </div>
+)
+
+export default Sidebar;
--- chat_message_app/src/pages/Home.js
+++ chat_message_app/src/pages/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Col, Grid, Row } from 'rsuite';
+import Sidebar from '../components/Sidebar';
 
 const Home = () => (
-        <div>
-            Home
-        </div>
-    )
+    <Grid fluid className= "h-100">
+        <Row>
+            <Col xs= {24} md= {8}>
+                <Sidebar/>
+            </Col>
+        </Row>
+    </Grid>
+)
 
 export default Home; 
