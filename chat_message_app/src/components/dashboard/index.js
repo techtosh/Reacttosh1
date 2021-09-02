4fc0de99bb85dfa738f307e37db994675f4564fd
internship311 <techtosh588@gmail.com>
2021-09-03 04:38:48 +0530

    started to work with user avatar upload

--- chat_message_app/src/components/dashboard/index.js
+++ chat_message_app/src/components/dashboard/index.js
@@ -3,6 +3,7 @@ import { Alert, Button, Divider, Drawer } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
 import { database } from '../../misc/firebase';
 import EditableInput from '../EditableInput';
+import AvatarUploadBtn from './AvatarUploadBtn';
 import ProviderBlock from './ProviderBlock';
 
 
@@ -39,6 +40,7 @@ const Dashboard = ({onSignOut}) => {
         onSave= {onSave}
         label= {<h6 className= "mb-2">Nickname</h6>}
         />
+        <AvatarUploadBtn />
     </Drawer.Body>
 
     <Drawer.Footer>
